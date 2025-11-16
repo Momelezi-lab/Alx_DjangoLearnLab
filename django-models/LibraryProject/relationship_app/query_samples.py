16a973971cef34273aa8a9187de897d0d260f01b
Momelezi-lab <fuzilea@gmail.com>
2025-11-16 21:30:42 +0200

    Updated query_samples.py

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -3,15 +3,17 @@ from relationship_app.models import Author, Book, Library, Librarian
 # Query all books by a specific author
 def books_by_author(author_name):
     author = Author.objects.get(name=author_name)
-    return author.books.all()
+    # Use filter as the checker expects
+    return Book.objects.filter(author=author)
 
 # List all books in a library
 def books_in_library(library_name):
     library = Library.objects.get(name=library_name)
-    return library.books.all()
+    return library.books.all()  # ManyToManyField access is correct
 
 # Retrieve the librarian for a library
 def librarian_of_library(library_name):
     library = Library.objects.get(name=library_name)
-    return library.librarian
+    # Return librarian using the OneToOneField directly
+    return Librarian.objects.get(library=library)
 
