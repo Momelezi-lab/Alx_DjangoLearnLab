132864ae0883436aaf6164327ef13623b9483adc
Momelezi-lab <fuzilea@gmail.com>
2025-11-17 20:10:36 +0200

    Phase 4 of the project completed

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import list_books, LibraryDetailView  # ALX checker requires this exact import
+from .views import admin_view, librarian_view, member_view
 
-urlpatterns = [
-    path('books/', list_books, name='list_books'),
-    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+urlpatterns += [
+    path('admin-view/', admin_view, name='admin_view'),
+    path('librarian-view/', librarian_view, name='librarian_view'),
+    path('member-view/', member_view, name='member_view'),
 ]
 
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,20 +1,28 @@
-from django.shortcuts import render
-from django.views.generic.detail import DetailView
-from .models import Book, Library
-from .models import Library  # Required by ALX checker
+kfrom django.shortcuts import render
+from django.contrib.auth.decorators import user_passes_test
 
-# Function-based view: list all books
-def list_books(request):
-    books = Book.objects.all()
-    return render(
-        request,
-        "relationship_app/list_books.html",   # checker expects this exact path
-        {"books": books}
-    )
+from .models import UserProfile
 
-# Class-based view: show details for a specific library
-class LibraryDetailView(DetailView):
-    model = Library
-    template_name = "relationship_app/library_detail.html"  # checker expects this exact path
-    context_object_name = "library"
+# Check functions
+def is_admin(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
+
+def is_librarian(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Librarian'
+
+def is_member(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Member'
+
+# Role-based views
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, "relationship_app/admin_view.html")
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, "relationship_app/librarian_view.html")
+
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, "relationship_app/member_view.html")
 
