606c856e9f6aa26c94b2d0fcd028f567bd9c623e
Momelezi-lab <fuzilea@gmail.com>
2025-11-17 20:19:11 +0200

    added new files

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import admin_view, librarian_view, member_view
+from .views import add_book, edit_book, delete_book
 
 urlpatterns += [
-    path('admin-view/', admin_view, name='admin_view'),
-    path('librarian-view/', librarian_view, name='librarian_view'),
-    path('member-view/', member_view, name='member_view'),
+    path('book/add/', add_book, name='add_book'),
+    path('book/<int:pk>/edit/', edit_book, name='edit_book'),
+    path('book/<int:pk>/delete/', delete_book, name='delete_book'),
 ]
 
