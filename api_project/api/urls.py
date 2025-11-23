fab8ed10b7bf0d33736c6b87f79576e4c3e6093b
Momelezi-lab <fuzilea@gmail.com>
2025-11-24 00:50:01 +0200

    phase 3

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import BookList
 
 urlpatterns = [
-    path('books/', BookList.as_view(), name='book-list'),
+    path('books/', BookList.as_view(), name='book-list'),  # existing ListAPIView
+    path('', include(router.urls)),                        # all CRUD endpoints via router
 ]
