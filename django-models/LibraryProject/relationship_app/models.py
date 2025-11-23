6d0eadd3040cf16afe40408181d880c7f33108d3
Momelezi-lab <fuzilea@gmail.com>
2025-11-24 00:35:29 +0200

    Initial Django project setup with DRF, created 'api' app, added Book model, and ran migrations

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,26 +1,12 @@
-from django.db import models
-from django.contrib.auth.models import User
-
-class Author(models.Model):
-    name = models.CharField(max_length=255)
-
 class Book(models.Model):
     title = models.CharField(max_length=255)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
-    publication_year = models.IntegerField(null=True, blank=True)
+    publication_year = models.IntegerField()
 
     class Meta:
-        permissions = (
+        permissions = [
             ("can_add_book", "Can add book"),
             ("can_change_book", "Can change book"),
             ("can_delete_book", "Can delete book"),
-        )
-
-class Library(models.Model):
-    name = models.CharField(max_length=255)
-    books = models.ManyToManyField(Book)
-
-class Librarian(models.Model):
-    name = models.CharField(max_length=255)
-    library = models.OneToOneField(Library, on_delete=models.CASCADE)
+        ]
 
