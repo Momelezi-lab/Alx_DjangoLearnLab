68562219f03d986a1bfc6b403aa0a0485247e9ed
Momelezi-lab <fuzilea@gmail.com>
2025-12-07 15:48:44 +0200

    Task 1

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -12,3 +12,11 @@ class Post(models.Model):
 
     class Meta:
         ordering = ['-published_date']
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(max_length=500, blank=True)
+    profile_picture = models.ImageField(upload_to="profile_pics/", blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.user.username}'s Profile"
