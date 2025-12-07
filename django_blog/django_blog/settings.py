0c0c4594853ca68a35ad74b056f0eb3a2e61090c
Momelezi-lab <fuzilea@gmail.com>
2025-12-07 15:22:44 +0200

    Fixed errors

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -45,6 +45,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'USER' '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
