8d38d350c2bf62e6a06d6f59fce53f7243260645
Momelezi-lab <fuzilea@gmail.com>
2025-12-13 22:51:14 +0200

    Fixed incorrect files

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,7 +1,9 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
+from rest_framework.authtoken.models import Token
 
 User = get_user_model()
+
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -16,6 +18,7 @@ class UserSerializer(serializers.ModelSerializer):
         ]
         read_only_fields = ['followers']
 
+
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
 
@@ -24,9 +27,10 @@ class RegisterSerializer(serializers.ModelSerializer):
         fields = ['username', 'email', 'password']
 
     def create(self, validated_data):
-        user = User.objects.create_user(
+        user = get_user_model().objects.create_user(
             username=validated_data['username'],
             email=validated_data.get('email'),
             password=validated_data['password']
         )
+        Token.objects.create(user=user)
         return user
--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -3,31 +3,31 @@ from django.shortcuts import render
 # Create your views here.
 
 from rest_framework import generics, permissions
-from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
-from django.contrib.auth import authenticate
 from rest_framework.views import APIView
+from rest_framework.authtoken.models import Token
+from django.contrib.auth import authenticate, get_user_model
 
-from .serializers import UserSerializer, RegisterSerializer
-from django.contrib.auth import get_user_model
-
-
+from .serializers import RegisterSerializer, UserSerializer
 
 User = get_user_model()
 
 
 class RegisterView(generics.CreateAPIView):
-    queryset = User.objects.all()
     serializer_class = RegisterSerializer
+    permission_classes = [permissions.AllowAny]
 
     def create(self, request, *args, **kwargs):
-        response = super().create(request, *args, **kwargs)
-        user = User.objects.get(username=response.data['username'])
-        token, _ = Token.objects.get_or_create(user=user)
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        token = Token.objects.get(user=user)
+
         return Response({
-            'user': response.data,
+            'user': UserSerializer(user).data,
             'token': token.key
         })
+
 
 class LoginView(APIView):
     permission_classes = [permissions.AllowAny]
@@ -37,22 +37,16 @@ class LoginView(APIView):
         password = request.data.get('password')
 
         user = authenticate(username=username, password=password)
+        if not user:
+            return Response({'error': 'Invalid credentials'}, status=400)
 
-        if user:
-            token, _ = Token.objects.get_or_create(user=user)
-            return Response({
-                'token': token.key
-            })
+        token, _ = Token.objects.get_or_create(user=user)
+        return Response({'token': token.key})
 
-        return Response(
-            {'error': 'Invalid credentials'},
-            status=400
-        )
-    
 
 class ProfileView(generics.RetrieveUpdateAPIView):
     serializer_class = UserSerializer
     permission_classes = [permissions.IsAuthenticated]
 
     def get_object(self):
-        return self.request.user    
+        return self.request.user
